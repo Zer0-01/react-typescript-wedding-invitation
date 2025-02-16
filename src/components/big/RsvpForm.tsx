b6b08f0e5c16b77ebd9c22167182b9fe5fb30f19
Zer0-01 <anas.zulkifli2001@gmail.com>
2025-02-16 11:13:20 +0800

    fix: radio button

--- src/components/big/RsvpForm.tsx
+++ src/components/big/RsvpForm.tsx
@@ -19,33 +19,61 @@ const RsvpForm = () => {
 
     const [showModal, setShowModal] = useState(false);
 
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
+
+
     return (
         <>
             <Card className="shadow-sm py-3 ">
                 <Container>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                        <Form.Group className="mb-4" controlId="formBasicName">
                             <Form.Label>Your name</Form.Label>
-                            <Form.Control type="text" onChange={(e) => setForm({ ...form, name: e.target.value })} />
+                            <Form.Control required type="text" onChange={(e) => setForm({ ...form, name: e.target.value })} />
+                            <Form.Control.Feedback type="invalid">Please enter your name.</Form.Control.Feedback>
                         </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPhoneNumber">
+                        <Form.Group className="mb-4" controlId="formBasicPhoneNumber">
                             <Form.Label>Phone number</Form.Label>
-                            <Form.Control type="text" placeholder="+60123456789" onChange={(e) => setForm({ ...form, phoneNumber: e.target.value })} />
+                            <Form.Control required type="text" placeholder="+60123456789" onChange={(e) => setForm({ ...form, phoneNumber: e.target.value })} />
+                            <Form.Control.Feedback type="invalid">Please enter your phone number.</Form.Control.Feedback>
                         </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicAttendance">
-                            <Form.Label>Will you be attending</Form.Label>
-                            <div />
-                            <Form.Check id="radio-1" inline type="radio" label="Yes, I'll be there" name="group1" onChange={() => setForm({ ...form, attendance: true })}
-                            />
-                            <Form.Check id="radio-2" inline type="radio" label="Sorry, I can't make it" name="group1" onChange={() => setForm({ ...form, attendance: false, guestCount: 0 })}
-                            />
+                        <Form.Group className="mb-4">
+                            <div className="mb-2">Will you be attending?</div>
+                            <div key="inline-radio" className="mb-3">
+                                <Form.Check
+                                    inline
+                                    label="Yes, I'll be there"
+                                    name="attendance"
+                                    type="radio"
+                                    id="attendYes"
+                                    onChange={() => setForm({ ...form, attendance: true })}
+                                />
+                                <Form.Check
+                                    inline
+                                    label="Sorry, I can't make it"
+                                    name="attendance"
+                                    type="radio"
+                                    id="attendNo"
+                                    onChange={() => setForm({ ...form, attendance: false, guestCount: 0 })}
+                                />
+                            </div>
                         </Form.Group>
 
-                        {!!form.attendance && <Form.Group className="mb-3" controlId="formBasicGuest">
+                        {!!form.attendance && <Form.Group className="mb-4" controlId="formBasicGuest">
                             <Form.Label>Additional guest (Maximum: 5)</Form.Label>
-                            <Form.Select aria-label="Default select example" onChange={(e) => setForm({ ...form, guestCount: parseInt(e.target.value) })}>
+                            <Form.Select required aria-label="Default select example" onChange={(e) => setForm({ ...form, guestCount: parseInt(e.target.value) })}>
                                 <option value="0">No additional guest</option>
                                 <option value="1">1</option>
                                 <option value="2">2</option>
@@ -55,20 +83,18 @@ const RsvpForm = () => {
                             </Form.Select>
                         </Form.Group>}
 
-                        <Form.Group className="mb-3" controlId="formBasicMessage">
+                        <Form.Group className="mb-4" controlId="formBasicMessage">
                             <Form.Label>Wishes for the couple</Form.Label>
                             <Form.Control as="textarea" rows={5} placeholder="Share your wishes for the couple" onChange={(e) => setForm({ ...form, message: e.target.value })} />
                         </Form.Group>
 
                         <Row className="justify-content-center">
                             <Col xs="auto">
-                                <Button className="text-center" variant="primary" type="button" onClick={() => setShowModal(true)}>
+                                <Button className="text-center" variant="primary" type="submit" >
                                     Submit
                                 </Button>
                             </Col>
-
                         </Row>
-
                     </Form>
                 </Container>
             </Card >
@@ -88,11 +114,15 @@ const RsvpForm = () => {
                     <div>Please review your RSVP details. Once submitted, you cannot modify this information.</div>
                     <br />
                     <div className="fw-bold">Your RSVP details:</div>
-                    <RsvpSummary label={"Name"} value={form.name} />
-                    <RsvpSummary label={"Phone number"} value={form.phoneNumber} />
-                    <RsvpSummary label={"Will you be attending"} value={form.attendance ? "Yes, I'll be there" : "Sorry, I can't make it"} />
-                    <RsvpSummary label={"Additional guest"} value={form.guestCount.toString()} />
-                    <RsvpSummary label={"Wishes for the couple"} value={form.message} />
+                    {[
+                        { label: "Name", value: form.name },
+                        { label: "Phone number", value: form.phoneNumber },
+                        { label: "Will you be attending", value: form.attendance ? "Yes, I'll be there" : "Sorry, I can't make it" },
+                        { label: "Additional guest", value: form.guestCount.toString() },
+                        { label: "Wishes for the couple", value: form.message }
+                    ].map((item, index) => (
+                        <RsvpSummary key={index} label={item.label} value={item.value} />
+                    ))}
 
                 </Modal.Body>
                 <Modal.Footer>
