538c5c23e9286e29ba0aebd20fc25e6ee918953f
Zer0-01 <anas.zulkifli2001@gmail.com>
2025-02-24 22:34:09 +0800

    feat: handle send message

--- src/components/MessageComponent.tsx
+++ src/components/MessageComponent.tsx
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react";
-import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Form, Row, Spinner } from "react-bootstrap";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore/lite";
+import { toast, ToastContainer } from "react-toastify";
+import { db } from "../FirebaseConfig";
+
+enum MessageStatus {
+    INITIAL, LOADING, SUCCESS, FAILURE
+}
 
 const MessageComponent = () => {
     const [disabled, setDisabled] = useState<boolean>(true);
     const [name, setName] = useState<string>("");
     const [message, setMessage] = useState<string>("");
+    const [status, setStatus] = useState<MessageStatus>(MessageStatus.INITIAL);
 
     useEffect(() => {
         if (name && message && name !== "" && message !== "") {
@@ -14,55 +22,81 @@ const MessageComponent = () => {
         }
     }, [name, message]);
 
+    const handleSend = async () => {
+        setStatus(MessageStatus.LOADING);
+        try {
+            await addDoc(collection(db, "message"), {
+                name: name,
+                message: message,
+                timestamp: serverTimestamp()
+            });
+            setStatus(MessageStatus.SUCCESS);
+            showToast("Message data sent successfully!");
+            console.log("RSVP data sent successfully!");
+        } catch (error) {
+            setStatus(MessageStatus.FAILURE);
+            showToast("Error sending message data");
+            console.error("Error sending RSVP data:", error);
+        }
+
+    }
+
+    const showToast = (message: string) => toast(message);
 
     return (
-        <Container
-            fluid
-            style={{
-                backgroundColor: "#FFFAEC",
-            }}>
-            <Row className="py-5">
-                <Col>
-                    <Card>
-                        <Card.Body>
-                            <Card.Title>
-                                Send a Message
-                            </Card.Title>
-                            <Form>
-                                <Form.Group
-                                    className="mb-3"
-                                    controlId="formNameMessage">
-                                    <Form.Label>Name</Form.Label>
-                                    <Form.Control
-                                        type="text"
-                                        placeholder="Enter your name"
-                                        onChange={(e) => setName(e.target.value)}
-                                    />
-                                </Form.Group>
+        <>
+            <Container
+                fluid
+                style={{
+                    backgroundColor: "#FFFAEC",
+                }}>
+                <Row className="py-5">
+                    <Col>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>
+                                    Send a Message
+                                </Card.Title>
+                                <Form>
+                                    <Form.Group
+                                        className="mb-3"
+                                        controlId="formNameMessage">
+                                        <Form.Label>Name</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="Enter your name"
+                                            onChange={(e) => setName(e.target.value)}
+                                        />
+                                    </Form.Group>
 
-                                <Form.Group
-                                    className="mb-3"
-                                    controlId="formMessage">
-                                    <Form.Label>Message</Form.Label>
-                                    <Form.Control
-                                        as="textarea"
-                                        type="text"
-                                        placeholder="Enter your message"
-                                        rows={5}
-                                        onChange={(e) => setMessage(e.target.value)}
-                                    />
-                                </Form.Group>
+                                    <Form.Group
+                                        className="mb-3"
+                                        controlId="formMessage">
+                                        <Form.Label>Message</Form.Label>
+                                        <Form.Control
+                                            as="textarea"
+                                            type="text"
+                                            placeholder="Enter your message"
+                                            rows={5}
+                                            onChange={(e) => setMessage(e.target.value)}
+                                        />
+                                    </Form.Group>
 
-                                <Button
-                                    disabled={disabled}
-                                    
-                                >Send</Button>
-                            </Form>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
+                                    <Button
+                                        disabled={disabled || status === MessageStatus.LOADING}
+                                        onClick={handleSend}
+                                    >
+                                        {status === MessageStatus.LOADING ? <Spinner as="span" animation="border" size="sm" /> : "Send"}
+                                    </Button>
+                                </Form>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+            <ToastContainer />
+        </>
+
     );
 };
 
