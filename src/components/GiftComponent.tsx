b6fe9f007962d24cb9fec1198b7f35f944a51d13
Zer0-01 <anas.zulkifli2001@gmail.com>
2025-02-27 21:12:09 +0800

    feat: create the gift item component

--- src/components/GiftComponent.tsx
+++ src/components/GiftComponent.tsx
@@ -4,6 +4,10 @@ import Row from "react-bootstrap/esm/Row"
 import Card from "react-bootstrap/esm/Card";
 import { AiOutlineCopy } from "react-icons/ai";
 import { toast } from "react-toastify";
+import { Button, Form, Spinner } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore/lite";
+import { db } from "../FirebaseConfig";
 
 
 
@@ -12,10 +16,21 @@ interface CardDetail {
     name: string;
     bankName: string;
     accountNumber: string;
+}
 
+interface Gift {
+    name: string;
+    phone: string;
+    isSelected: boolean;
+}
+
+enum GiftStatus {
+    INITIAL, LOADING, SUCCESS, FAILURE
 }
 
 const GiftComponent = () => {
+    const [status, setStatus] = useState<GiftStatus>(GiftStatus.INITIAL);
+    const [giftList, setGiftList] = useState<Gift[]>([]);
     const cardDetailList: CardDetail[] = [
         {
             name: "Anas Zulkifli bin Mohd Jeffry",
@@ -29,12 +44,30 @@ const GiftComponent = () => {
         }
     ]
 
+    useEffect(() => {
+        fetchGifts();
+    }, []);
+
     const handleCopy = (accountNumber: string) => {
         navigator.clipboard.writeText(accountNumber);
         showToast(`Account number copied to clipboard!: ${accountNumber}`);
     }
 
     const showToast = (message: string) => toast(message);
+
+    const fetchGifts = async () => {
+        setStatus(GiftStatus.LOADING);
+        try {
+            const querySnapshot = await getDocs(collection(db, "gift"));
+            querySnapshot.forEach((gift) => {
+                setGiftList((prevGiftList) => [...prevGiftList, gift.data() as Gift]);
+            });
+            setStatus(GiftStatus.SUCCESS);
+        } catch (error) {
+            setStatus(GiftStatus.FAILURE);
+            console.error("Error fetching gifts:", error);
+        }
+    }
 
     return (
         <>
@@ -61,6 +94,45 @@ const GiftComponent = () => {
                         </Col>
                     </Row>
                 ))}
+                {status === GiftStatus.LOADING
+                    ? <Spinner as="span" animation="border" size="sm" />
+                    : <Row className="pb-5">
+                        <Col>
+                            <Form>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Phone Number</Form.Label>
+                                    <Form.Control type="text" placeholder="Enter phone number" />
+                                </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Gift</Form.Label>
+                                    <Form.Select>
+                                        {giftList.map((gift, index) => (
+                                            <option key={index} value={gift.name}>{gift.name}</option>
+                                        ))}
+                                    </Form.Select>
+                                </Form.Group>
+                                <Button>Send</Button>
+                            </Form>
+                        </Col>
+                    </Row>}
+                <Row className="pb-5">
+                    <Col>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>List of Gifts</Card.Title>
+                                <Container
+                                    className="overflow-auto p-0"
+                                    style={{ maxHeight: "50vh" }}>
+                                    <Row>
+                                        <Col>
+
+                                        </Col>
+                                    </Row>
+                                </Container>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
             </Container>
         </>
     );
