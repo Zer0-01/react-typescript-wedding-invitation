e7c9049e40d65e34a43f787d80cba9aab25bac96
Zer0-01 <anas.zulkifli2001@gmail.com>
2025-03-16 11:19:31 +0800

    feat: animate the card for form

--- src/sections/rsvpForm/RsvpFormSection.tsx
+++ src/sections/rsvpForm/RsvpFormSection.tsx
@@ -5,6 +5,7 @@ import { addDoc, collection, serverTimestamp } from "firebase/firestore/lite";
 import { toast } from "react-toastify";
 import RsvpModal from "./RsvpModal";
 import { colorPrimary } from "../../constants/ColorsConstant";
+import { motion } from "framer-motion";
 
 export enum RsvpStatus {
     INITIAL, LOADING, SUCCESS, FAILURE
@@ -68,81 +69,90 @@ const RsvpFormSection = () => {
                 }>
                 <Row className="py-5">
                     <Col>
-                        <Card>
-                            <Card.Body>
-                                <Card.Title>RSVP Form</Card.Title>
-                                <Form>
-                                    <Form.Group
-                                        className="mb-3"
-                                        controlId="formName">
-                                        <Form.Label>Name</Form.Label>
-                                        <Form.Control
-                                            type="text"
-                                            placeholder="Enter your name"
-                                            onChange={(e) => setName(e.target.value)} />
-                                    </Form.Group>
-
-                                    <Form.Group
-                                        className="mb-3"
-                                        controlId="formPhoneNumber">
-                                        <Form.Label>Phone Number</Form.Label>
-                                        <Form.Control
-                                            type="text"
-                                            placeholder="Enter your phone number"
-                                            onChange={(e) => setPhone(e.target.value)}
-                                        />
-                                    </Form.Group>
-
-                                    <Form.Group className="mb-3" controlId="formAttendance">
-                                        <Form.Label>Will you be attending?</Form.Label>
-                                        <Form.Check
-                                            type="radio"
-                                            id="attending-yes"
-                                            label="I will"
-                                            name="default-radio"
-                                            onChange={() => {
-                                                setAttendance(true);
-                                                setGuest(1);
-                                            }}
-                                        />
-                                        <Form.Check
-                                            type="radio"
-                                            id="attending-no"
-                                            label="I will not"
-                                            name="default-radio"
-                                            onChange={() => {
-                                                setAttendance(false);
-                                                setGuest(0);
-                                            }}
-                                        />
-                                    </Form.Group>
-                                    {attendance &&
-                                        <Form.Group className="mb-3" controlId="formGuest">
-                                            <Form.Label>Number of guest(s)</Form.Label>
-                                            <Form.Select onChange={(e) => setGuest(parseInt(e.target.value))}>
-                                                <option value={1}>1</option>
-                                                <option value={2}>2</option>
-                                                <option value={3}>3</option>
-                                                <option value={4}>4</option>
-                                                <option value={5}>5</option>
-                                            </Form.Select>
+                        <motion.div
+                            initial={{ scale: 0.8, opacity: 0 }}
+                            whileInView={{ scale: 1, opacity: 1 }}
+                            transition={{ duration: 0.6, ease: "easeOut" }}
+                            viewport={{ once: true }}
+                        >
+                            <Card>
+                                <Card.Body>
+                                    <Card.Title>RSVP Form</Card.Title>
+                                    <Form>
+                                        <Form.Group
+                                            className="mb-3"
+                                            controlId="formName">
+                                            <Form.Label>Name</Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                placeholder="Enter your name"
+                                                onChange={(e) => setName(e.target.value)} />
                                         </Form.Group>
-                                    }
+
+                                        <Form.Group
+                                            className="mb-3"
+                                            controlId="formPhoneNumber">
+                                            <Form.Label>Phone Number</Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                placeholder="Enter your phone number"
+                                                onChange={(e) => setPhone(e.target.value)}
+                                            />
+                                        </Form.Group>
+
+                                        <Form.Group className="mb-3" controlId="formAttendance">
+                                            <Form.Label>Will you be attending?</Form.Label>
+                                            <Form.Check
+                                                type="radio"
+                                                id="attending-yes"
+                                                label="I will"
+                                                name="default-radio"
+                                                onChange={() => {
+                                                    setAttendance(true);
+                                                    setGuest(1);
+                                                }}
+                                            />
+                                            <Form.Check
+                                                type="radio"
+                                                id="attending-no"
+                                                label="I will not"
+                                                name="default-radio"
+                                                onChange={() => {
+                                                    setAttendance(false);
+                                                    setGuest(0);
+                                                }}
+                                            />
+                                        </Form.Group>
+                                        {attendance &&
+                                            <Form.Group className="mb-3" controlId="formGuest">
+                                                <Form.Label>Number of guest(s)</Form.Label>
+                                                <Form.Select onChange={(e) => setGuest(parseInt(e.target.value))}>
+                                                    <option value={1}>1</option>
+                                                    <option value={2}>2</option>
+                                                    <option value={3}>3</option>
+                                                    <option value={4}>4</option>
+                                                    <option value={5}>5</option>
+                                                </Form.Select>
+                                            </Form.Group>
+                                        }
 
 
-                                    <Button
-                                        disabled={disabled}
-                                        onClick={() => setShowModal(true)}
-                                        style={{
-                                            backgroundColor: colorPrimary[500],
-                                            borderColor: colorPrimary[500],
-                                        }}
-                                    >
-                                        {status === RsvpStatus.LOADING ? <Spinner as="span" animation="border" size="sm" /> : "Send"}
-                                    </Button>
-                                </Form>
-                            </Card.Body>
-                        </Card>
+                                        <Button
+                                            disabled={disabled}
+                                            onClick={() => setShowModal(true)}
+                                            style={{
+                                                backgroundColor: colorPrimary[500],
+                                                borderColor: colorPrimary[500],
+                                            }}
+                                        >
+                                            {status === RsvpStatus.LOADING ? <Spinner as="span" animation="border" size="sm" /> : "Send"}
+                                        </Button>
+                                    </Form>
+                                </Card.Body>
+                            </Card>
+
+                        </motion.div>
+
                     </Col>
                 </Row>
             </Container>
