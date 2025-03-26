347ce98c4c45bf9e0f4d5cb46f893784dfd2e745
Zer0-01 <anas.zulkifli2001@gmail.com>
2025-03-27 00:14:21 +0800

    feat: complete the title section

--- src/atul/sections/CardCoupleAtulSection.tsx
+++ src/atul/sections/CardCoupleAtulSection.tsx
@@ -19,66 +19,40 @@ const CardCoupleAtulSection = () => (
             fluid>
             <Row className="g-0 h-100">
                 <Col sm={0} md={2} lg={3} xl={4} className="d-none d-md-block" />
-                <Col sm={12} md={8} lg={6} xl={4}>
-                    <Row className="py-5">
-                        <Col className="text-center">
-                            <div className="fs-1">Assalammualaikum</div>
-                        </Col>
-                    </Row>
-                    <Row className="pb-3">
-                        <Col className="text-center">
-                            <div className="fs-5">Dengan memohon rahmat dan ridho Allah Subhanahu Wa Ta'ala, Kami mengundang Bapak/Ibu/Saudara/i, untuk menghadiri Resepsi Pernikahan kami.</div>
-                        </Col>
-                    </Row>
+                <Col sm={12} md={8} lg={6} xl={4} className="d-flex flex-column">
+                    <div className="fs-2 pt-5 text-center">Assalamualaikum wbt</div>
+                    <div className="fs-2 pb-3 text-center">& Salam Sejahtera</div>
+                    <div className="fs-6 text-center">Dengan penuh kesyukuran, kami menjemput</div>
+                    <div className="fw-bold text-center fs-6">Dato' | Datin | Tuan | Puan | Encik | Cik</div>
+                    <div className="fs-6 text-center pb-5">Ke majlis perkahwinan anakanda kami</div>
 
-                    <Row>
-                        <Col className="text-center">
-                            <Image
-                                style={{
-                                    backgroundColor: "#e6e6e6"
-                                }}
-                                className="w-50"
-                                src="../src/assets/atul-cartoon.png"
-                                roundedCircle
-                                fluid />
-                        </Col>
-                    </Row>
-                    <Row className="pt-2">
-                        <Col className="text-center">
-                            <div>Nur Izzatul Khairiah</div>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col className="text-center">
-                            <div>Puteri kepada Encik Mubin dan Puan Zoliana</div>
-                        </Col>
-                    </Row>
+                    <div className="text-center">
+                        <Image
+                            style={{
+                                backgroundColor: "#e6e6e6",
+                                width: "40%"
+                            }}
 
+                            src="../src/assets/atul-cartoon.png"
+                            roundedCircle
+                            fluid /></div>
+                    <div className="pt-2 text-center">Nur Izzatul Khairiah</div>
+                    <div className="text-center">Puteri kepada Encik Mubin dan Puan Zoliana</div>
+                    <div className="text-center py-5 fst-italic">dengan pasangannya</div>
+                    <div className="text-center">
+                        <Image
+                            style={{
+                                backgroundColor: "#e6e6e6",
+                                width: "40%"
+                            }}
 
+                            src="../src/assets/anas-cartoon.png"
+                            roundedCircle
+                            fluid />
+                    </div>
+                    <div className="pt-2 text-center">Anas Zulkifli</div>
+                    <div className="pb-5 text-center">Putera kepada Encik Mohd Jeffry dan Puan Jamilah</div>
 
-
-                    <Row className="pt-3">
-                        <Col className="text-center">
-                            <Image
-                                style={{
-                                    backgroundColor: "#e6e6e6"
-                                }}
-                                className="w-50"
-                                src="../src/assets/anas-cartoon.png"
-                                roundedCircle
-                                fluid />
-                        </Col>
-                    </Row>
-                    <Row className="pt-2">
-                        <Col className="text-center">
-                            <div>Anas Zulkifli</div>
-                        </Col>
-                    </Row>
-                    <Row className="pb-5">
-                        <Col className="text-center">
-                            <div>Putera kepada Encik Mohd Jeffry dan Puan Jamilah</div>
-                        </Col>
-                    </Row>
 
                 </Col>
                 <Col sm={0} md={2} lg={3} xl={4} className="d-none d-md-block" />
