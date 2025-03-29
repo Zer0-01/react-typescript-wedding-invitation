33c4bc280e34e6e62dfd1bc735813fedaa0a9a54
Zer0-01 <anas.zulkifli2001@gmail.com>
2025-03-30 01:40:17 +0800

    refactor: clean countdown and place

--- src/anas/sections/CountdownSection.tsx
+++ src/anas/sections/CountdownSection.tsx
@@ -1,99 +1,64 @@
-import { motion } from "framer-motion";
 import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import { Card, Col, Container, Row } from "react-bootstrap";
 import { colorBrown } from "../../constants/ColorsConstant";
 
-const CountdownSection = () => {
-    const calculateTimeLeft = (): Record<string, number> => {
-        const targetDate = new Date("2025-05-18T11:00:00");
-        const now = new Date();
-        const difference = targetDate.getTime() - now.getTime();
+// Helper function to calculate the time left
+const calculateTimeLeft = (): Record<string, number> => {
+    const targetDate = new Date("2025-05-18T11:00:00");
+    const now = new Date();
+    const difference = targetDate.getTime() - now.getTime();
 
-        let timeLeft: Record<string, number> = { D: 0, H: 0, M: 0, S: 0 };
-        if (difference > 0) {
-            timeLeft = {
-                D: Math.floor(difference / (1000 * 60 * 60 * 24)),
-                H: Math.floor((difference / (1000 * 60 * 60)) % 24),
-                M: Math.floor((difference / 1000 / 60) % 60),
-                S: Math.floor((difference / 1000) % 60),
-            };
+    return difference > 0
+        ? {
+            D: Math.floor(difference / (1000 * 60 * 60 * 24)),
+            H: Math.floor((difference / (1000 * 60 * 60)) % 24),
+            M: Math.floor((difference / 1000 / 60) % 60),
+            S: Math.floor((difference / 1000) % 60),
         }
-        return timeLeft;
-    };
+        : { D: 0, H: 0, M: 0, S: 0 };
+};
 
-    const [timeLeft, setTimeLeft] = useState<Record<string, number>>(calculateTimeLeft());
-
+const CountdownSection = () => {
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
     useEffect(() => {
-        const timer = setInterval(() => {
-            setTimeLeft(calculateTimeLeft());
-        }, 1000);
+        const timer = setInterval(() => setTimeLeft(calculateTimeLeft()), 1000);
         return () => clearInterval(timer);
     }, []);
 
     return (
-        <Container fluid
-            className=" p-0"
-            style={{ backgroundColor: "#f3efe4" }}
-        >
-            <Row className="g-0 h-100">
-                <Col sm={0} md={2} lg={3} xl={4} className="d-none d-md-block" />
-                <Col sm={12} md={8} lg={6} xl={4} >
-                    <Row className="py-5 px-2 g-0">
-                        <Col>
-                            <Card
-                                className="border-0"
-                                style={{
-                                    backgroundColor: "white",
-                                }}
-                            >
-                                <Card.Body>
-                                    <Row className="pb-3">
-                                        <Col>
-                                            <Card.Text className="text-center fs-2">Countdown</Card.Text>
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        {Object.entries(timeLeft).map(([unit, value], index) => (
-                                            <Col xs={3} key={index}>
-                                                <motion.div
-                                                    initial={{ opacity: 0, y: 100 }}
-                                                    whileInView={{ opacity: 1, y: 0 }}
-                                                    transition={{ duration: 2, delay: index * 0.5 }}
-                                                    viewport={{ once: true }}
-                                                >
-                                                    <Card
-                                                        className="border-0"
-                                                        style={{
-                                                            backgroundColor: colorBrown[100]
-                                                        }}>
-                                                        <Card.Body>
-                                                            <Row>
-                                                                <Col>
-                                                                    <div className="text-center fs-2">{unit}</div>
-                                                                </Col>
-                                                            </Row>
-                                                            <Row>
-                                                                <Col>
-                                                                    <div className="text-center fs-2">{value}</div>
-                                                                </Col>
-                                                            </Row>
-                                                        </Card.Body>
-                                                    </Card>
-                                                </motion.div>
-
-                                            </Col>
-                                        ))}
-                                    </Row>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    </Row>
+        <Container fluid style={{ backgroundColor: "#f3efe4" }}>
+            <Row className="g-0 pt-5 pb-2 h-100 justify-content-center">
+                <Col sm={12} md={8} lg={6} xl={4}>
+                    <Card className="border-0 bg-white">
+                        <Card.Body>
+                            <Card.Text className="text-center fs-2">Countdown</Card.Text>
+                            <Row>
+                                {Object.entries(timeLeft).map(([unit, value], index) => (
+                                    <Col xs={3} key={unit}>
+                                        <motion.div
+                                            initial={{ opacity: 0, y: 100 }}
+                                            whileInView={{ opacity: 1, y: 0 }}
+                                            transition={{ duration: 2, delay: index * 0.5 }}
+                                            viewport={{ once: true }}
+                                        >
+                                            <Card className="border-0" style={{ backgroundColor: colorBrown[100] }}>
+                                                <Card.Body>
+                                                    <div className="text-center fs-2">{unit}</div>
+                                                    <div className="text-center fs-2">{value}</div>
+                                                </Card.Body>
+                                            </Card>
+                                        </motion.div>
+                                    </Col>
+                                ))}
+                            </Row>
+                        </Card.Body>
+                    </Card>
                 </Col>
-                <Col sm={0} md={2} lg={3} xl={4} className="d-none d-md-block" />
             </Row>
         </Container>
     );
-}
+};
 
 export default CountdownSection;
--- src/anas/sections/PlaceSection.tsx
+++ src/anas/sections/PlaceSection.tsx
@@ -1,65 +1,55 @@
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
-import { colorBrown } from "../../constants/ColorsConstant";
 import { motion } from "framer-motion";
+import { colorBrown } from "../../constants/ColorsConstant";
 
 const PlaceSection = () => {
     const handleClick = () => {
-        window.open("https://maps.app.goo.gl/6rBA7thVaVejn79x6")
-    }
+        window.open("https://maps.app.goo.gl/6rBA7thVaVejn79x6", "_blank");
+    };
 
     return (
-        <Container
-            style={{ backgroundColor: "#f3efe4" }}
-            fluid
-        >
-            <Row className="g-0 pb-5">
-                <Col sm={0} md={2} lg={3} xl={4} className="d-none d-md-block" />
-                <Col sm={12} md={8} lg={6} xl={4} >
+        <Container fluid style={{ backgroundColor: "#f3efe4" }}>
+            <Row className="g-0 pb-5 pt-2  justify-content-center">
+                <Col sm={12} md={8} lg={6} xl={4}>
                     <motion.div
                         initial={{ opacity: 0, y: -100 }}
                         whileInView={{ opacity: 1, y: 0 }}
                         viewport={{ once: true }}
                         transition={{ duration: 2 }}
-
                     >
                         <Card
+                            className="text-center p-5 border-0"
                             style={{
                                 backgroundImage: "url('../src/assets/background-card-place.jpg')",
                                 backgroundSize: "cover",
                                 backgroundPosition: "center",
                                 borderTopLeftRadius: "8rem",
                                 borderTopRightRadius: "8rem",
-                            }}>
-                            <Card.Body className="text-center p-5">
-                                <Card.Title className="text-center fs-1">Resepsi</Card.Title>
-                                <Card.Text className="text-center fs-5">Ahad, 18 Mei 2025</Card.Text>
-                                <Card.Text className="text-center fs-5">11.00 - 04.00 PM: Jamuan Makan</Card.Text>
-                                <Card.Text className="text-center fs-5">12.00 - 12.30 PM: Sanding</Card.Text>
-                                <Card.Text className="text-center fs-2">Marissa Grand Event Space</Card.Text>
-                                <Card.Text className="text-center">107-1, Jalan Khalidi, Taman Khalidi Bharu, 84000 Muar, Johor Darul Ta'zim</Card.Text>
+                            }}
+                        >
+                            <Card.Body>
+                                <Card.Title className="fs-1">Resepsi</Card.Title>
+                                <Card.Text className="fs-5">Ahad, 18 Mei 2025</Card.Text>
+                                <Card.Text className="fs-5">11.00 - 04.00 PM: Jamuan Makan</Card.Text>
+                                <Card.Text className="fs-5">12.00 - 12.30 PM: Sanding</Card.Text>
+                                <Card.Text className="fs-2 fw-bold">Marissa Grand Event Space</Card.Text>
+                                <Card.Text>
+                                    107-1, Jalan Khalidi, Taman Khalidi Bharu, 84000 Muar, Johor Darul Ta'zim
+                                </Card.Text>
                                 <Button
-                                    style={
-                                        {
-                                            backgroundColor: colorBrown[500],
-                                            borderColor: colorBrown[500],
-                                        }
-                                    }
                                     className="w-100"
+                                    style={{ backgroundColor: colorBrown[500], borderColor: colorBrown[500] }}
                                     onClick={handleClick}
                                 >
                                     Lokasi
                                 </Button>
                             </Card.Body>
-
                         </Card>
                     </motion.div>
                 </Col>
-                <Col sm={0} md={2} lg={3} xl={4} className="d-none d-md-block" />
             </Row>
-            <div style={{ height: "1px", backgroundColor: "black", width: "100%" }} />
-
+            <div style={{ height: "5px", backgroundColor: colorBrown[500], width: "100%" }} />
         </Container>
-
     );
 };
 
