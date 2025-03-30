c55ed3451d681555e08c6eb0cd792a44284b3945
Zer0-01 <anas.zulkifli2001@gmail.com>
2025-03-31 00:14:21 +0800

    feat: add song

--- src/anas/screens/HomeScreen.tsx
+++ src/anas/screens/HomeScreen.tsx
@@ -8,8 +8,34 @@ import MessageSection from '../sections/MessageSection';
 import GiftSection from '../sections/GiftSection';
 import { ToastContainer } from 'react-toastify';
 import FooterSection from '../sections/FooterSection';
+import { useEffect, useState } from 'react';
+import { HiSpeakerWave } from "react-icons/hi2";
+import { HiSpeakerXMark } from "react-icons/hi2";
+
+
 
 function HomeScreen() {
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [audio] = useState(new Audio('../src/assets/song.mp3'));
+
+  useEffect(() => {
+    audio.loop = true;
+    audio.play().then(() => setIsPlaying(true)).catch(() => console.log("Autoplay blocked"));
+    return () => {
+      audio.pause();
+    };
+  }, [audio])
+
+  const toggleSound = () => {
+    if (isPlaying) {
+      audio.pause();
+      setIsPlaying(false);
+    } else {
+      audio.play();
+      setIsPlaying(true);
+    }
+  }
+
   return (
     <>
       <TitleSection />
@@ -21,6 +47,15 @@ function HomeScreen() {
       <GiftSection />
       <FooterSection />
       <ToastContainer />
+      <div
+        className='position-fixed bottom-0 end-0 m-2 p-2 rounded-circle shadow '
+        style={{ backgroundColor: "white" }}
+        onClick={toggleSound}
+      >
+        {isPlaying ? (<HiSpeakerWave />) : (<HiSpeakerXMark />)}
+      </div>
+
+
     </>
   );
 }
