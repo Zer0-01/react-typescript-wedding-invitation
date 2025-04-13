4b25e221957779267126f4fc0aec985cfd870ca6
Zer0-01 <anas.zulkifli2001@gmail.com>
2025-04-13 22:31:27 +0800

    fix: layout issue for the landing screen

--- src/anas/screens/LandingScreen.tsx
+++ src/anas/screens/LandingScreen.tsx
@@ -21,23 +21,31 @@ const LandingScreen = () => {
                 exit={{ opacity: 0 }}
                 transition={{ duration: 0.5 }}
             >
-                <Row className="g-0 justify-content-center">
-                    <Col sm={12} md={8} lg={6} xl={4} >
+                <Row 
+                
+                className="g-0 justify-content-center"
+                style={{
+                    height: "80%",
+                }}
+                >
+                    <Col sm={12} md={8} lg={6} xl={4} className="h-100" >
                         <Image
                             src={background}
-                            className="h-100 object-fit-contain"
+                            className="h-100 w-100 object-fit-contain"
                             fluid
                         />
                     </Col>
                 </Row>
-                <div className="text-center">
+                <div className="text-center position-relative "
+                style={{
+                    height: "20%",
+                }}
+                >
                     <Button
-                        className="rounded-circle"
+                        className="position-absolute top-50 start-50 translate-middle rounded-circle"
                         style={{
-                            bottom: "10%",
                             backgroundColor: "#d38f5c",
                             border: "none",
-
                         }}
                         onClick={() => navigate("/home")}
                     >
