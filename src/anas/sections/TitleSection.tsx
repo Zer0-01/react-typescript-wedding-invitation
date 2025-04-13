7f82ec7a571de59bec7735cafed323735a2a8763
Zer0-01 <anas.zulkifli2001@gmail.com>
2025-04-13 22:54:13 +0800

    fix:  set the column also take height: 100%

--- src/anas/sections/TitleSection.tsx
+++ src/anas/sections/TitleSection.tsx
@@ -13,10 +13,11 @@ const TitleSection = () => {
         <Container fluid className="vh-100 p-0" style={{ backgroundColor: colorBackground }}>
             <motion.div {...fadeInFromTop} className="h-100">
                 <Row className="g-0 h-100 justify-content-center">
-                    <Col sm={12} md={8} lg={6} xl={4}>
+                    <Col sm={12} md={8} lg={6} xl={4}
+                    className="h-100">
                         <Image
                             src={background}
-                            className="h-100 object-fit-fill"
+                            className="h-100 object-fit-contain"
                             fluid
                         />
                     </Col>
